fe29649864f46faac7f1d9636fb3d4f9f70b1c24
Koen Schouten <schouten_koen@hotmail.com>
2022-07-14 15:43:15 +0200

    Setup observer pattern

--- js/app.js
+++ js/app.js
@@ -1,64 +1,49 @@
-const regexEvaluator = (function(){
+const regexEvaluator = (function () {
 
     let regex = new RegExp();
     let testStrings = [];
 
-    function setRegex(expr, flags="g"){
+    function setRegex(expr, flags = "g") {
         regex = new RegExp(expr, flags)
+        console.log(regex);
     }
 
-    function getRegex(){
+    function getRegex() {
         return regex;
     }
 
-    function setTestStrings(...strings){
+    function setTestStrings(...strings) {
         testStrings = strings;
         console.log(testStrings);
 
     }
 
-    function getTestStrings(...strings){
+    function getTestStrings(...strings) {
         return testStrings;
     }
 
-
-    function init(){
-        console.log("test");
+    function regexUpdatedHandler(origin) {
+        setRegex(origin.getRegexString());
     }
 
+    function testStringUpdatedHandler(origin) {
+        setTestStrings(origin, getTestStrings());
+    }
+
+    function init() {
+        console.log("test");
+    }
 
     return {
         "init": init,
         "setRegex": setRegex,
         "getRegex": getRegex,
-        "setTestStrings" : setTestStrings,
-        "getTestStrings" : getTestStrings
+        "setTestStrings": setTestStrings,
+        "getTestStrings": getTestStrings,
+        "regexUpdatedHandler": regexUpdatedHandler,
+        "testStringUpdatedHandler": testStringUpdatedHandler
     }
 })();
-
-function escapeHTML(unsafe){
-    return unsafe.replaceAll('&', '&amp;')
-        .replaceAll('<', '&lt;')
-        .replaceAll('>', '&gt;')
-        .replaceAll('"', '&quot;')
-        .replaceAll("'", '&#039;');
-}
-
-function init(){
-    regexEvaluator.init();
-
-    const regexInputElement = document.getElementById("regex-input");
-    const testStringsInputElement = document.getElementById("test-strings-input");
-    const testStringsOutputElement = document.getElementById("test-strings-output");
-
-    regexInputElement.addEventListener("input", (event) =>{
-        event.preventDefault();
-        regexEvaluator.setRegex(event.target.value);
-    })
-    setupTestStringsInputElement(testStringsInputElement, 
-                                    testStringsOutputElement)
-}
-
 
 // In order to have an Editable Textarea that can also be styled,
 // we need to create 2 seperate elements that are duplicates. 
@@ -66,43 +51,141 @@ function init(){
 // This code makes sure that these 2 elements are in sync,
 //
 // see: https://css-tricks.com/creating-an-editable-textarea-that-supports-syntax-highlighted-code/
-function setupTestStringsInputElement(testStringsInputElement, testStringsOutputElement ){
-    //When the user scrolls the textarea. Then the output should scroll as well.
-    function syncscroll(){
+const testStringsInput = (function () {
+    let testStringsInputElement, testStringsOutputElement;
+    const observers = []
+
+    function init(_testStringsInputElement, _testStringsOutputElement) {
+        testStringsInputElement = _testStringsInputElement;
+        testStringsOutputElement = _testStringsOutputElement;
+
+        testStringsOutputElement.innerHTML = escapeHTML(testStringsInputElement.value);
+
+        setupEventListeners();
+    }
+
+    function setupEventListeners() {
+        testStringsInputElement.addEventListener("input", (event) => {
+            event.preventDefault();
+            result = event.target.value;
+            if (result[result.length - 1] == "\n") { // If the last character is a newline character
+                result += " "; // Add a placeholder space character to the final line 
+            }
+
+            testStringsOutputElement.innerHTML = escapeHTML(result);
+            syncscroll();
+            _notifyObservers();
+        })
+
+        testStringsInputElement.addEventListener("scroll", (event) => {
+            syncscroll();
+        });
+    }
+
+    function escapeHTML(unsafe) {
+        return unsafe.replaceAll('&', '&amp;')
+            .replaceAll('<', '&lt;')
+            .replaceAll('>', '&gt;')
+            .replaceAll('"', '&quot;')
+            .replaceAll("'", '&#039;');
+    }
+
+    function syncscroll() {
         testStringsOutputElement.scrollTop = testStringsInputElement.scrollTop;
         testStringsOutputElement.scrollLeft = testStringsInputElement.scrollLeft;
     }
 
-    //escape all special HTML chars when copying to output.
+    function getTestStrings() {
+        return testStringsInputElement.value;
+    }
+
+    function setTestStringsOutputElement(value) {
+        testStringsOutputElement.innerHTML = value;
+    }
+
+    function registerObserver(fn) {
+        observers.push(fn);
+    }
+
+    function unregisterObserver(fn) {
+        this.observers = this.handlers.filter(item => item !== fn);
+    }
+
+    function _notifyObservers() {
+        observers.forEach(function (fn) {
+            fn.call(this, testStringsInput);
+        });
+    }
+
+    return {
+        "init": init,
+        "getTestStrings": getTestStrings,
+        "registerObserver": registerObserver,
+        "unregisterObserver": unregisterObserver
+
+    }
+})();
+
+const regexInput = (function () {
+    let regexInputElement;
+    const observers = []
+
+    function setupEventListeners() {
+        regexInputElement.addEventListener("input", (event) => {
+            event.preventDefault();
+            _notifyObservers();
+        })
+    }
+
+    function init(_regexInputElement) {
+        regexInputElement = _regexInputElement;
+        setupEventListeners();
+    }
+
+    function getRegexString() {
+        return regexInputElement.value
+    }
+
+    function registerObserver(fn) {
+        observers.push(fn);
+    }
+
+    function unregisterObserver(fn) {
+        this.observers = this.handlers.filter(item => item !== fn);
+    }
+
+    function _notifyObservers() {
+        observers.forEach(function (fn) {
+            fn.call(this, regexInput);
+        });
+    }
+
+    return {
+        "init": init,
+        "getRegexString": getRegexString,
+        "registerObserver": registerObserver,
+        "unregisterObserver": unregisterObserver
+    }
+})();
 
 
-    testStringsOutputElement.innerHTML = escapeHTML(testStringsInputElement.value);
+function init() {
+    regexEvaluator.init();
 
-    testStringsInputElement.addEventListener("input", (event) =>{
-        event.preventDefault();
-        result = event.target.value;
-        regexEvaluator.setTestStrings(result.split('\n'));
+    const regexInputElement = document.getElementById("regex-input");
+    const testStringsInputElement = document.getElementById("test-strings-input");
+    const testStringsOutputElement = document.getElementById("test-strings-output");
 
-        if(result[result.length-1] == "\n") { // If the last character is a newline character
-            result += " "; // Add a placeholder space character to the final line 
-          }
+    testStringsInput.init(testStringsInputElement, testStringsOutputElement);
+    regexInput.init(regexInputElement);
 
-        testStringsOutputElement.innerHTML = escapeHTML(result);
-        syncscroll();
-    })
-
-
-    testStringsInputElement.addEventListener("scroll", (event) =>{
-        syncscroll();
-    });
-
-
+    testStringsInput.registerObserver(regexEvaluator.testStringUpdatedHandler);
+    regexInput.registerObserver(regexEvaluator.regexUpdatedHandler);
 }
 
 //======================
 //StartingPoint of APP.
 //======================
-window.addEventListener("DOMContentLoaded", ()=>{
+window.addEventListener("DOMContentLoaded", () => {
     init();
-    
 })
